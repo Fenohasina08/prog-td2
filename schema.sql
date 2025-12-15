779833111ca54cf2293be484c407392866537e2c
fenohasina <fenohasinaherimamy@gmail.com>
2025-12-15 14:49:51 +0300

    feat: schema.sql [add]

--- schema.sql
+++ schema.sql
@@ -1,2 +1,16 @@
 CREATE TYPE continent AS ENUM ('AFRICA', 'EUROPA', 'ASIA', 'AMERICA');
 CREATE TYPE position AS ENUM ('GK', 'DEF', 'MIDF', 'STR');
+
+CREATE TABLE Team (
+                      id SERIAL PRIMARY KEY,
+                      name VARCHAR(100) NOT NULL,
+                      continent continent_type NOT NULL
+);
+
+CREATE TABLE Player (
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(250) NOT NULL,
+                        age INT NOT NULL,
+                        position position_type NOT NULL,
+                        id_team INT REFERENCES Team(id) ON DELETE SET NULL
+);
